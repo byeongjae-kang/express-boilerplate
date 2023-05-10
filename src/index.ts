a0a548ab5bf9ec3ae916c7666434f705dca03b72
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-05-10 02:31:32 -0700

    remove console.log

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { app } from "./app";
 
 async function bootstrap() {
   /** connect to mongodb and more here */
-  console.log(MONGODB_URI, MONGODB_NAME);
   try {
     await mongoose.connect(`${MONGODB_URI}/${MONGODB_NAME}`);
     console.log(`[Mongoose]: Connected to ${MONGODB_NAME} database!`);
